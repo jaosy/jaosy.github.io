49d9a752bb06a52810db4a3fe8d8b2ddbc768c37
Jacqueline Ong <jong@macalester.edu>
2022-01-21 23:43:57 -0600

    (WIP) popup with more information about project

--- src/Projects/ProjectCard.js
+++ src/Projects/ProjectCard.js
@@ -1,13 +1,35 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Collapse from '@mui/material/Collapse';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContentText from '@mui/material/DialogContentText';
 
 
 export default function ProjectCard(props) {
+    const [open, setOpen] = useState(false);
+    const [selectedProject, setSelectedProject] = useState('');
+
+    const onClickMore = () => {
+        setOpen(true);
+    }
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    const generateParagraphs = (projectDescription) => {
+        console.log(projectDescription);
+        if (projectDescription !== '')
+            projectDescription.map(paragraph => <Typography paragraph='true'>paragraph</Typography>)
+    }
+
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
@@ -24,6 +46,15 @@ export default function ProjectCard(props) {
                     {props.project.blurb}
                 </Typography>
             </CardContent>
-        </Card>
+            <CardActions>
+                <Button size="small" onClick={onClickMore}>More</Button>
+            </CardActions>
+
+            <Dialog open={open}
+                onClose={onClose}>
+                <DialogTitle id="scroll-dialog-title">{props.project.title}</DialogTitle>
+                <DialogContentText sx={{ p: '1.5em' }}>{() => { generateParagraphs(props.project.description) }}</DialogContentText>
+            </Dialog>
+        </Card >
     );
 }
--- src/Projects/ProjectsData.js
+++ src/Projects/ProjectsData.js
@@ -2,7 +2,12 @@ export const projectsData = [
     {
         title: "Blocks4All",
         image: "https://milnel2.github.io/blocks4alliOS/img/blocks4all.png",
-        blurb: "Accessible blocks-based programming iPad application"
+        blurb: "Accessible blocks-based programming iPad application",
+        description: [
+            "Blocks-based programming environments are a popular tool to teach children how to code, but they are often inaccessible for children with visual impairments who cannot interact with their visual components. I was part of a research lab that worked on Blocks4All, a touchscreen-based blocks-based programming environment that is accessible with screen readers and tries to bridge this gap in accessible computer science education.",
+
+            "We added more complex functionality such variable and functions, and improved the user interface to be more accessible with Apple's VoiceOver, Dynamic Type (scalable text) and better palettes for users with color blindness."
+        ]
     },
     {
         title: "FidgetWidget",
